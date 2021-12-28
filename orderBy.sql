865ece0c5a75e2d43207ada4ebe48151baec0305
Allysson Santos <allyssonsantos487@gmail.com>
2021-12-28 20:53:15 -0300

    01

--- orderBy.sql
+++ orderBy.sql
@@ -2,3 +2,10 @@
 SELECT coluana1,coluna2
 FROM tabela 
 ORDER BY coluna1 asc/desc
+
+
+-- Ordenar por Nome e sobrenome 
+
+SELECT FirstName, LastName
+FROM person.person
+ORDER BY FirstName asc , LastName desc
