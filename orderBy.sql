4f1b51c0bf9982939c44fd03aaf88e30b8072560
Allysson Santos <allyssonsantos487@gmail.com>
2021-12-28 21:20:44 -0300

    01

--- orderBy.sql
+++ orderBy.sql
@@ -21,3 +21,15 @@ DICAS-
 SELECT TOP 10 productId
 FROM Production.product
 ORDER BY listprice desc
+
+--DESAFIO 1
+/* Obter o nome e numero do produto dos produtos que tem o ProductId entr q~4
+-dicas
+-você terá que usar a tabela Production.product
+-você tera que uar o ORDER BY e Top
+-e para ordenar você terá que usar ORDER BY ASC ou DESC deprmdendo d resultado que está buscando
+*/
+
+SELECT TOP 4 name,productnumber
+FROM prooduction.product
+ORDER BY ProductId
