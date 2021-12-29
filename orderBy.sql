5c0a0e7c80bbccab783c995537300221b4e79375
Allysson Santos <allyssonsantos487@gmail.com>
2021-12-28 21:13:46 -0300

    01

--- orderBy.sql
+++ orderBy.sql
@@ -8,4 +8,16 @@ ORDER BY coluna1 asc/desc
 
 SELECT FirstName, LastName
 FROM person.person
-ORDER BY FirstName asc , LastName desc
+
+
+-- DESAFIO 1
+/*
+Obeter o ProductId dos 10 produtos mais caros cadastrados no sistema, listando do mais caro para o mais barato
+DICAS-
+-Você terá que usar a tabela Production.product
+-Você terá que usar ORDER BY e TOP
+-E para ordeernar você terá que usar o ORDER BY ASC ou DEC dependendo do resultado que está buscano
+*/
+SELECT TOP 10 productId
+FROM Production.product
+ORDER BY listprice desc
