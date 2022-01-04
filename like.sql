708b17a673ac689692ebaaf8335cebfd113f003c
Allysson Santos <allyssonsantos487@gmail.com>
2022-01-03 21:16:45 -0300

    10

--- like.sql
+++ like.sql
@@ -1,1 +1,6 @@
 --# Aluna 10 Like
+-- Vamos dizer que você quer encontrar uma pessoa no banco de dados que vc sabe que o nome dela era ovi... alguma  coisa
+
+SELECT *
+FROM person.Person 
+WHERE FirstName Like 'ovi%'
